ee99b654cc75aa28305015036eb36f41739ed820
sophiehajjar <sophieh111@hotmail.com>
2023-03-01 15:50:11 -0500

    Creation of database
    The database was created, we are able to modify elements in it but can't seem to be able to get data from it. An issue has been allocated for this bug.

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -1,15 +1,23 @@
+const { MongoClient, ObjectId} = require("mongodb");
 const express = require('express');
 const res = require("express/lib/response");
 const router = express.Router();
 
 
-router.get('/:id', function(req, res,next){
-    res.send('whats up');
+const client = new MongoClient("mongodb+srv://MindHack:MindHack123$@cluster0.sdwjjsx.mongodb.net/test");
+
+
+router.get('/:id', async function (req, res, next) {
+    const db = client.db("MindHack");
+    let body = {"username" : "tamere", "password" : "raasd", "email" : "allo@tamere.com"};
+    const users = await db.collection("user").find({_id: new ObjectId("63fe6789ab517b13701d43cb")},
+        {
+            $set: body
+        });
+
+    console.log(users);
+    res.status(200).json(users);
     next();
-    })
-
-
-
-
+});
 
 module.exports = router;
