d1b8386cff0ec305d3d70c957a9b4cb79584e78b
carlocostacc <carlo.costa1@hotmail.com>
2023-03-10 11:53:31 -0500

    Changes to be committed: 	new file:   back-end/.env 	deleted:    back-end/app.js 	renamed:    back-end/routes/job.js -> back-end/controllers/test.js 	new file:   back-end/db/conn.js 	modified:   back-end/package-lock.json 	modified:   back-end/package.json 	new file:   back-end/routes/record.js 	deleted:    back-end/routes/user.js 	new file:   back-end/server.js 	modified:   front-end/MindHack/package-lock.json 	modified:   front-end/MindHack/package.json 	modified:   front-end/MindHack/src/App.js

---
+++ back-end/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+app.use(morgan("dev"));
+console.log(process.env.ATLAS_URI)
+mongoose.connect(process.env.ATLAS_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("connection successful"))
+.catch((err) => console.log("connection failed", err));
+
+app.use(cors({origin:true, credentials: true}));
--- front-end/MindHack/src/App.js
+++ front-end/MindHack/src/App.js
@@ -8,7 +8,16 @@ import Jobs from "./jobs";
 import Homepage from "./homepage";
 
 function App() {
-
+  const { MongoClient, ServerApiVersion } = require('mongodb');
+  const uri = "mongodb+srv://carlocosta1:<TRlKq6oVy3RHX5tC>@cluster0.mdevfeh.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+  client.connect(err => {
+    const collection = client.db("test").collection("Jobpostings");
+    // perform actions on the collection object
+  
+    client.close();
+  });
+  
   return (
       <div>
        <Navbar/>
