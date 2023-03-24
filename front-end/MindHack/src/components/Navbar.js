1bb0c0d1fe6ccaa8f5e6b2891da11469b8cc9b6f
laraxl <laraconcordia1@gmail.com>
2023-03-24 11:37:12 -0400

    Job Posting completed
    The job posting has been completed and couple tweaks here and there

--- front-end/MindHack/src/components/Navbar.js
+++ front-end/MindHack/src/components/Navbar.js
@@ -23,11 +23,12 @@ export default function Navbar()  {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         Mind Hack
                     </Typography>
-                    <Button color="inherit"> <NavLink to={"/"}>Homepage </NavLink></Button>
-                    <Button color="inherit"> <NavLink to={"/jobbs"}>New job </NavLink></Button>
+                    <Button color="inherit"> <NavLink to={"/homepage"}>Homepage </NavLink></Button>
+                    <Button color="inherit"> <NavLink to={"/jobs"}>New job </NavLink></Button>
                     <Button color="inherit"> <NavLink to={"/login"}>Login</NavLink></Button>
                     <Button color="inherit"> <NavLink to={"/profile"}>Profile</NavLink></Button>
-
+                    <Button color="inherit"> <NavLink to={"/employer_post"}>Employee post</NavLink></Button>
+                    {/*<Button color="inherit"> <NavLink to={"/Appp"}>Appp</NavLink></Button>*/}
 
                 </Toolbar>
             </AppBar>
