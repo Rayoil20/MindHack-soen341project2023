16fb2ec8b4e63a3af0becf5552701ab6c6b5de06
Ryan K <ryankattoura26@gmail.com>
2023-03-23 22:24:33 -0400

    Completion of the looks and the page
    The page is a form that is filled out to modify anything in the profile

--- front-end/MindHack/src/App.jsx
+++ front-end/MindHack/src/App.jsx
@@ -6,6 +6,7 @@ import {dividerClasses} from "@mui/material";
 import Navbar from "./components/Navbar";
 import Jobs from "./jobs";
 import Homepage from "./homepage";
+import Edit_Profile from "./edit_profile";
 
 function App() {
   
@@ -18,6 +19,7 @@ function App() {
            <Route path='/' element={<Homepage/>}/>
            <Route path='/profile' element={<Profile/>}/>
            <Route path='/job' element={<Jobs/>}/>
+           <Route path='/edit_profile' element={<Edit_Profile/>}/>
        </Routes>
       </div>
   );
