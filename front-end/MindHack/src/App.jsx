47f7a7a3004fd3fa1cee83a837ed34383d651aec
sophiehajjar <41131337+sophiehajjar@users.noreply.github.com>
2023-04-12 11:39:12 -0400

    Revert "Job page update"

--- front-end/MindHack/src/App.jsx
+++ front-end/MindHack/src/App.jsx
@@ -1,15 +1,11 @@
 import logo from './logo.svg';
 import React, { useState } from "react";
 import Profile from "./profile";
-import {Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import {dividerClasses} from "@mui/material";
 import Navbar from "./components/Navbar";
 import Jobs from "./jobs";
-import Homepage from "./jobs";
-import Employer_Post from "./employer_posting";
-import {BrowserRouter} from "react-router-dom";
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-
+import Homepage from "./homepage";
 
 function App() {
   
@@ -21,10 +17,7 @@ function App() {
        <Routes>
            <Route path='/' element={<Homepage/>}/>
            <Route path='/profile' element={<Profile/>}/>
-           <Route path='/jobs' element={<Jobs/>}/>
-           <Route path='/employer_post' element={<Employer_Post/>}/>
-
-
+           <Route path='/job' element={<Jobs/>}/>
        </Routes>
       </div>
   );
