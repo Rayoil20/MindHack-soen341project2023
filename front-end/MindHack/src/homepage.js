8b3e6f88d5ef3ef59e81ceca9d161a84908233dd
laraxl <laraconcordia1@gmail.com>
2023-04-20 00:57:52 -0400

    Backend for job posting linked to the front

--- front-end/MindHack/src/homepage.js
+++ front-end/MindHack/src/homepage.js
@@ -1,12 +1,31 @@
 import React from "react";
+import {Typography} from '@mui/material';
 
 
-export default function Homepage(props) {
-
+export default function Homepage() {
     return (
-        <div>Homepage</div>
-
-    )
-
-
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            backgroundColor: '#F7F7F7',
+        }}>
+            <Typography variant="h1" style={{
+                fontWeight: 700,
+                fontSize: '3rem',
+                marginBottom: '1rem',
+            }}>
+                Welcome to Mind Hack
+            </Typography>
+            <Typography variant="h2" style={{
+                fontWeight: 400,
+                fontSize: '1.5rem',
+                textAlign: 'center',
+            }}>
+                Find your next career opportunity today.
+            </Typography>
+        </div>
+    );
 }
